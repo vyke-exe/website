fcaaf8d88760ae482b6c85246bd4d1be03c000d7
vyke-exe <epsi0@tutanota.com>
2025-06-05 23:49:13 -0400

    fixing urls

--- _config.ts
+++ _config.ts
@@ -1,5 +1,4 @@
 import lume from "lume/mod.ts";
-import basePath from "lume/plugins/base_path.ts";
 
 const site = lume();
 
@@ -9,7 +8,5 @@ site.add("/img");
 site.add("/gallery");
 site.add("/fonts");
 site.add("/buttons");
-
-site.use(basePath());
 
 export default site;
